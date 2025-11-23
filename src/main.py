139b1ab5ca34ec36c52b9444d945c7cf779aff5a
jrestrepo <jrestrepo@uner.edu.ar>
2025-11-23 11:23:43 -0300

    Added initial files

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import os
+
+from dash import Dash, Input, Output, callback, dcc, html
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
+app = Dash()
+
+app.layout = [
+    html.H1(children="Title of Dash App", style={"textAlign": "center"}),
+]
+
+
+def main():
+    app_name = os.getenv("APP_NAME")
+    debug_mode = os.getenv("DEBUG_MODE")
+    local_host = os.getenv("HOST")
+    local_port = os.getenv("PORT")
+    print(f"APP_NAME: {app_name}")
+    print(f"DEBUG_MODE: {debug_mode}")
+    app.run(debug=debug_mode, host=local_host, port=local_port)
+
+
+if __name__ == "__main__":
+    main()
